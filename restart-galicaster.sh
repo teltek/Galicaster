dc68068d212e267e9324d1da5a6c7656dc5dd776
Cato Kolås <edpck@uib.no>
2012-06-05 11:18:28 +0200

    now uses args to run_galicaster.py

--- restart-galicaster.sh
+++ restart-galicaster.sh
@@ -6,4 +6,4 @@ pkill -u mca python
 sleep 5
 
 # start galicaster again
-/opt/galicaster-uib/run_galicaster.py &
+/opt/galicaster-uib/run_galicaster.py -c ~mca/.galicaster/`hostname`.conf.ini -d ~mca/.galicaster/conf-dist.ini &
