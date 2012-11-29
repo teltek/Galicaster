8cb64889068d933bc57c1a73b45254c0d8e94de1
Olav Bringedal <Olav.Bringedal@adm.uib.no>
2012-11-29 11:17:14 +0100

    fikset restart scriptert iht ny merge

--- restart-galicaster.sh
+++ restart-galicaster.sh
@@ -6,4 +6,4 @@ pkill -u mca python
 sleep 5
 
 # start galicaster again
-/opt/galicaster/run_galicaster.py -c ~mca/.galicaster/`hostname`.conf.ini -d ~mca/.galicaster/conf-dist.ini &
+/opt/galicaster/run_galicaster.py &
