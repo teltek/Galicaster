8f30fe9f8a186102b878c2d6d51012ffb78d1d24
Anders Myren <amy098@mca6.(none)>
2012-12-21 10:34:05 +0100

    endra restartscript

--- restart-galicaster.sh
+++ restart-galicaster.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # kill galicaster
-pkill -u mca python
+echo "" >> /tmp/restart-galicaster.log
+echo "----Running restart script----"  >> /tmp/restart-galicaster.log
+date >> /tmp/restart-galicaster.log
+echo "Killing Galicaster"
+pkill -u $USER python
 
-sleep 5
+echo "Waiting 20 sec for network to be established"
+sleep 20
 
 # start galicaster again
+echo "Restarting Galicaster"
 /opt/galicaster/run_galicaster.py &
