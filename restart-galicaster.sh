6ae7b5f21e206fd243e55cc8776c7ff6f9a10d81
Anders Myren <amy098@mh.uib.no>
2013-01-31 09:47:17 +0100

    Moved logging to save it after reboots, starting rtsp2v4l2

--- restart-galicaster.sh
+++ restart-galicaster.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # kill galicaster
-echo ""
-echo "----Running restart script----> `date`"  
+echo "----Running restart script----> `date`"  >> /var/log/galicaster/restart-galicaster.log
 date
 echo "Killing Galicaster"
 pkill -u $USER python
 
-echo "Checking if rtsp2v4l2 is running"
-/opt/galicaster/rtsp_keeprunning.sh
-
 echo "Waiting 10 sec for network to be established"
+sleep 10
+
+echo "Checking if rtsp2v4l2 is running"
+/opt/galicaster/rtsp_keeprunning.sh >> /var/log/galicaster/rtsp_keeprunning.log
+
+echo "Waiting 10 sec for rtsp2v4l2 to be established"
 sleep 10
 
 # start galicaster again
 echo "Restarting Galicaster"
-/opt/galicaster/run_galicaster.py &
+/opt/galicaster/run_galicaster.py & >> /var/log/galicaster/restart-galicaster.log
