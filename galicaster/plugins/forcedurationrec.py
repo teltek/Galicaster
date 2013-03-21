4384fd4a2d90a43ef7d6da975c04b49a448391f1
rubenrua <rubenrua@teltek.es>
2013-03-21 16:17:14 +0100

    Update forcedurationrec plugin

--- galicaster/plugins/forcedurationrec.py
+++ galicaster/plugins/forcedurationrec.py
@@ -12,52 +12,48 @@
 # San Francisco, California, 94105, USA.
 
 """
-TODO. 
- - Key en stop_recording
- - Timer solo grabaciones manuales???
- - Comprobar si la grabacion es manual o automatica para empezar o no el Timer 
- - Añadir log
- - Hacer QA
+TODO:
+ - Timer only in manual recordings
+ - Doc
 """
 from threading import Timer
 from threading import _Timer
 
 from galicaster.core import context
 
+
+dispatcher = context.get_dispatcher()
+conf = context.get_conf()
+logger = context.get_logger()
+
 t_stop = None
+max_duration = conf.get_int('forcedurationrec', 'duration') or 300
+
 
 def init():
-    dispatcher = context.get_dispatcher()
     dispatcher.connect("galicaster-notify-quit", do_stop_timers)
     dispatcher.connect('starting-record', create_timer)
-
-    try:
-        hours = int(conf.get('forcedurationrec','duration'))
-    except ValueError:
-        #log or set default value
-        pass
+    dispatcher.connect('restart-preview', do_stop_timers)
 
 
-def create_timer():
-    if isinstance(t_stop, _Timer):
-        t_stop.cancel()
-
-    try:
-        hours = int(conf.get('forcedurationrec','duration'))
-    except ValueError:
-        #log or set default value
-        pass
-
-    t_stop = Timer(60 * hours, stop_recording) 
+def create_timer(sender=None):
+    global t_stop
+    do_stop_timers()
+    t_stop = Timer(60 * max_duration, stop_recording) 
     t_stop.start()
-    
+    logger.debug("Init Timer to stop a record in {} minutes".format(max_duration))
+
 
 def stop_recording(sender=None):
+    global t_stop
     t_stop = None
-    dispatcher = context.get_dispatcher()
-    #dispatcher.emit("stop-record", key) #TODO Complete key
+    dispatcher.emit('stop-record', 0)
+    logger.info("Forceduration plugin stops the recording".format(max_duration))
 
 
 def do_stop_timers(sender=None):
+    global t_stop
     if isinstance(t_stop, _Timer):
         t_stop.cancel()
+        logger.debug("Reset Timer")
+
