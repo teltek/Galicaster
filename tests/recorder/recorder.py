e276d658ad36b2f3f54399e53ee3c11cfdcb4b60
Ruben Gonzalez <rubenrua@gmail.com>
2014-06-12 22:19:34 +0200

    New not async recorder test

--- tests/recorder/recorder.py
+++ tests/recorder/recorder.py
@@ -15,13 +15,21 @@
 Tests for `galicaster.recorder.recorder` modules.
 """
 
+from os import path
+import gtk
 import time
+from shutil import rmtree
+from tempfile import mkdtemp
 from unittest import TestCase
+from gst.pbutils import Discoverer
 from galicaster.recorder.recorder import Recorder
+
+gtk.gdk.threads_init()
 
 
 class TestFunctions(TestCase):
     def setUp(self):
+        self.tmppath = mkdtemp()
         self.default_bins = [{'name': 'Bars', 
                               'pattern': '0',
                               'caps': 'video/x-raw-yuv,framerate=25/1,width=640,height=480',
@@ -31,7 +39,7 @@ class TestFunctions(TestCase):
                               'file': 'CAMERA.avi',
                               'device': 'videotest',
                               'flavor': 'presenter',
-                              'path': '/tmp'},
+                              'path': self.tmppath},
                              {'name': 'Static',
                               'device': 'videotest',
                               'location': 'default',
@@ -41,7 +49,7 @@ class TestFunctions(TestCase):
                               'pattern': '1',
                               'color1': '4294967295',
                               'color2': '4278190080',
-                              'path': '/tmp'},
+                              'path': self.tmppath},
                              {'name': 'Noise',
                               'device': 'audiotest',
                               'location': 'default',
@@ -53,8 +61,12 @@ class TestFunctions(TestCase):
                               'player': 'True',
                               'vumeter': 'True',
                               'amplification': '1.0',
-                              'path': ' /tmp'}]
+                              'path': self.tmppath}]
                              
+
+    def tearDown(self):
+        rmtree(self.tmppath)
+
 
     def test_constructor(self):
         recorder = Recorder(self.default_bins)
@@ -65,26 +77,95 @@ class TestFunctions(TestCase):
         self.assertRaises(KeyError, Recorder, [{'device': 'v4l2', 'caps': 'raw'}])
 
         #valid
-        Recorder([{'name': 'test', 'device': 'v4l2', 'path': '/tmp'}])
+        Recorder([{'name': 'test', 'device': 'v4l2', 'path': self.tmppath}])
 
 
     def test_get_display_areas_info(self):
         recorder = Recorder(self.default_bins)
         self.assertEqual(recorder.get_display_areas_info(), ['sink-Bars', 'sink-Static'])
-        recorder = Recorder([{'name': '1', 'device': 'videotest', 'path': '/tmp'}, 
-                             {'name': '2', 'device': 'videotest', 'path': '/tmp'}])
+        recorder = Recorder([{'name': '1', 'device': 'videotest', 'path': self.tmppath}, 
+                             {'name': '2', 'device': 'videotest', 'path': self.tmppath}])
         self.assertEqual(recorder.get_display_areas_info(), ['sink-1', 'sink-2'])
 
 
     def test_get_bins_info(self):
-        recorder = Recorder([{'name': 'name', 'device': 'videotest', 'path': '/tmp'}])
+        recorder = Recorder([{'name': 'name', 'device': 'videotest', 'path': self.tmppath}])
         info = recorder.get_bins_info()
         self.assertEqual(info[0]['name'], 'name')
-        self.assertEqual(info[0]['path'], '/tmp')
+        self.assertEqual(info[0]['path'], self.tmppath)
 
 
     def test_preview(self):
-        recorder = Recorder([{'name': 'name', 'device': 'videotest', 'path': '/tmp'}])
+        recorder = Recorder([{'name': 'name', 'device': 'videotest', 'path': self.tmppath, 'file': '1.avi'}])
         recorder.preview()
         time.sleep(2)
-        recorder.stop_preview()
+        recorder.stop()
+        self.assertEqual(recorder.get_recorded_time(), 0)
+        self.assertEqual(path.getsize(path.join(self.tmppath, '1.avi')), 0)
+
+
+    def test_preview_multi(self):
+        recorder = Recorder([{'name': '1', 'device': 'videotest', 'path': self.tmppath, 'file': '1.avi', 'pattern': '1'},
+                             {'name': '2', 'device': 'videotest', 'path': self.tmppath, 'file': '2.avi'},
+                             {'name': '3', 'device': 'audiotest', 'path': self.tmppath, 'file': '3.mp3'}])
+        recorder.preview()
+        time.sleep(4)
+        recorder.stop()
+        self.assertEqual(recorder.get_recorded_time(), 0)
+        self.assertEqual(path.getsize(path.join(self.tmppath, '1.avi')), 0)
+        self.assertEqual(path.getsize(path.join(self.tmppath, '2.avi')), 0)
+        self.assertEqual(path.getsize(path.join(self.tmppath, '3.mp3')), 0)
+
+
+    def test_preview_and_record(self):
+        recorder = Recorder([{'name': 'name', 'device': 'videotest', 'path': self.tmppath, 'file': '1.avi'}])
+        recorder.preview_and_record()
+        time.sleep(2)
+        recorder.stop()
+        self.assertTrue(recorder.get_recorded_time() > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '1.avi')) > 0)
+
+
+    def test_preview_and_record_multi(self):
+        recorder = Recorder([{'name': '1', 'device': 'videotest', 'path': self.tmppath, 'file': '1.avi', 'pattern': '1'},
+                             {'name': '2', 'device': 'videotest', 'path': self.tmppath, 'file': '2.avi'},
+                             {'name': '3', 'device': 'audiotest', 'path': self.tmppath, 'file': '3.mp3'}])
+        recorder.preview_and_record()
+        time.sleep(4)
+        recorder.stop()
+        self.assertTrue(recorder.get_recorded_time() > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '1.avi')) > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '2.avi')) > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '3.mp3')) > 0)
+
+
+    def test_record(self):
+        recorder = Recorder([{'name': 'name', 'device': 'videotest', 'path': self.tmppath, 'file': '1.avi'}])
+        recorder.preview()
+        time.sleep(2)
+        rec_time = recorder.get_recorded_time()
+        #self.assertEqual(rec_time, 0)
+        recorder.record()
+        time.sleep(2)
+        recorder.stop()
+        self.assertTrue(recorder.get_recorded_time() > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '1.avi')) > 0)
+        d = Discoverer()
+        print d.discover_uri_async(path.join(self.tmppath, '1.avi'))
+
+
+    def test_record_multi(self):
+        recorder = Recorder([{'name': '1', 'device': 'videotest', 'path': self.tmppath, 'file': '1.avi', 'pattern': '1'},
+                             {'name': '2', 'device': 'videotest', 'path': self.tmppath, 'file': '2.avi'},
+                             {'name': '3', 'device': 'audiotest', 'path': self.tmppath, 'file': '3.mp3'}])
+        recorder.preview()
+        time.sleep(4)
+        rec_time = recorder.get_recorded_time()
+        #self.assertEqual(recorder.rec_time, 0)
+        recorder.record()
+        time.sleep(4)
+        recorder.stop()
+        self.assertTrue(recorder.get_recorded_time() > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '1.avi')) > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '2.avi')) > 0)
+        self.assertTrue(path.getsize(path.join(self.tmppath, '3.mp3')) > 0)
