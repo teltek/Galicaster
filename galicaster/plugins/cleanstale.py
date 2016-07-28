9f81ce506873883accd508d6cf8a974efd0ee9f1
Daniel Vilar <daniel.peiteado@teltek.es>
2016-07-28 13:43:13 +0200

    Clean code in cleanstale

--- galicaster/plugins/cleanstale.py
+++ galicaster/plugins/cleanstale.py
@@ -6,9 +6,9 @@
 # Copyright (c) 2012, Teltek Video Research <galicaster@teltek.es>
 #
 # This work is licensed under the Creative Commons Attribution-
-# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of 
-# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 171 Second Street, Suite 300, 
+# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
 """
@@ -16,6 +16,9 @@
 """
 from galicaster.core import context
 
+conf = None
+days = None
+logger = None
 
 def init():
     global days, logger
@@ -30,14 +33,14 @@ def init():
         days = int(conf.get('cleanstale', 'maxarchivaldays'))
     except Exception as exc:
         raise Exception("Wrong parameter maxarchivaldays: {}".format(exc))
-    
+
     if days:
         logger.info("Parameter 'maxarchivaldays' set to {} days".format(days))
         dispatcher.connect('timer-nightly', clear_job)
-        
+
         oninit = conf.get('cleanstale', 'checkoninit')
         if oninit in ["True", "true"]:
-            clear_job(None)        
+            clear_job(None)
     else:
         raise Exception("Parameter maxarchivaldays not configured")
 
@@ -54,5 +57,3 @@ def clear_job(sender=None):
     for mp in mps:
         logger.info("Removing MP {}".format(mp.getIdentifier()))
         repo.delete(mp)
-    
-
