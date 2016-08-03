cee9dfd4072a3d6c3150ff26112607fc356ddfb0
Hugo Caloto <hcaloto@teltek.es>
2016-08-03 17:33:07 +0200

    Recorder service: change initializing for init (it was too long for the UI)

--- galicaster/recorder/service.py
+++ galicaster/recorder/service.py
@@ -29,7 +29,7 @@ class Status(object):
     def __repr__(self): return self.name
 
 
-INIT_STATUS      = Status('init', 'Initialization')
+INIT_STATUS      = Status('init', 'Init')
 PREVIEW_STATUS   = Status('preview', 'Waiting')
 RECORDING_STATUS = Status('recording', 'Recording', '#484848', '#FF0000')
 PAUSED_STATUS    = Status('paused', 'Paused')
