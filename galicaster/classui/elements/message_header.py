e5d85ed87dc1ca4b37829586ca3ea376b67cbc9f
dpeite <daniel.peiteado@teltek.es>
2016-09-16 17:32:10 +0200

    Refactor image in exit dialog

--- galicaster/classui/elements/message_header.py
+++ galicaster/classui/elements/message_header.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2011, Teltek Video Research <galicaster@teltek.es>
 #
 # This work is licensed under the Creative Commons Attribution-
-# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of 
-# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 171 Second Street, Suite 300, 
+# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 """
 A Custom taskbar or header for Galicaster messages, dialogs and other popups
@@ -16,17 +16,17 @@ from galicaster.classui import get_image_path
 class Header(Gtk.Frame):
 
     def __init__(self, size=[1920,1080],title = None):
-        
+
         wprop = size[0]/1920.0
         hprop = size[1]/1080.0
         Gtk.Frame.__init__(self)
         box = Gtk.HBox()
         box.set_border_width(int(wprop*10))
         strip = Gtk.Image()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file(get_image_path('logo.svg')) 
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file(get_image_path('logo.svg'))
         pixbuf = pixbuf.scale_simple(
-            int(pixbuf.get_width()*wprop*0.6),
-            int(pixbuf.get_height()*wprop*0.6),
+            int(pixbuf.get_width()*wprop*0.2),
+            int(pixbuf.get_height()*wprop*0.2),
             GdkPixbuf.InterpType.BILINEAR)
         strip.set_from_pixbuf(pixbuf)
         strip.set_alignment(0.05,0.5)
@@ -39,4 +39,3 @@ class Header(Gtk.Frame):
             box.pack_end(label, True, True, 0) # introduce hprop
         self.add(box)
         box.show_all()
-        
