52dec57cec8e6fb9bd9b96aaf31cdb044f87e27f
Alfonso Rodríguez <arodriguez@teltek.es>
2016-11-09 13:26:49 +0100

    Updated version for 2.0.0-RC2

--- galicaster/__init__.py
+++ galicaster/__init__.py
@@ -6,12 +6,12 @@
 # Copyright (c) 2011, Teltek Video Research <galicaster@teltek.es>
 #
 # This work is licensed under the Creative Commons Attribution-
-# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of 
-# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 171 Second Street, Suite 300, 
+# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
-__version__ = '2.0.x'
+__version__ = '2.0.0-RC2'
 
 import gi
 gi.require_version('Gst', '1.0')
