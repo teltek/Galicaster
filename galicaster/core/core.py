a4c39d5f8183a9b5804021b41d276412e1a3e5a7
Alfonso Rodríguez <arodriguez@teltek.es>
2016-12-22 19:36:24 +0100

    RM-12784 run_galicaster: Workaround for KeyboardInterruption.
    There was a bug with Gtk.main() not raising a KeyboardInterrupt(SIGINT) exception
    This would cause Galicaster to stay open when pressing Ctrl+C
    https://bugzilla.gnome.org/show_bug.cgi?id=622084
    
    I added a workaround adding a handler that closes the Galicaster window

--- galicaster/core/core.py
+++ galicaster/core/core.py
@@ -6,9 +6,9 @@
 # Copyright (c) 2011, Teltek Video Research <galicaster@teltek.es>
 #
 # This work is licensed under the Creative Commons Attribution-
-# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of 
-# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 171 Second Street, Suite 300, 
+# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
 
@@ -53,7 +53,7 @@ class Main():
         self.modules = self.conf.get_modules()
         self.load_modules()
 
-    def load_modules(self):        
+    def load_modules(self):
         self.window = context.get_mainwindow()
         # Load plugins after loading the main window (fixes a problem with the plugin 'nocursor')
         plugins.init()
@@ -61,11 +61,11 @@ class Main():
         # Recorder
         self.recorder = RecorderClassUI()
         self.insert_page(self.recorder, 'REC')
-        
-        if 'scheduler' in self.modules:        
+
+        if 'scheduler' in self.modules:
             self.scheduler = context.get_scheduler()
 
-        if 'ocservice' in self.modules:        
+        if 'ocservice' in self.modules:
             self.ocservice = context.get_ocservice()
 
         if 'media_manager' in self.modules:
@@ -73,12 +73,12 @@ class Main():
 
             # Distribution
             self.distribution = DistribUI()
-            self.insert_page(self.distribution, 'DIS') 
+            self.insert_page(self.distribution, 'DIS')
 
             # Media Manager
             self.listing  = ListingClassUI()
             self.insert_page(self.listing, 'MMA')
-        
+
         if 'player' in self.modules:
             self.player = PlayerClassUI()
             self.insert_page(self.player, 'PLA')
@@ -94,11 +94,11 @@ class Main():
         else:
             logger.info("Set {} as home page, modules loaded: {}".format(homepage, self.modules))
             self.window.set_current_page(PAGES[homepage])
-            
+
         context.get_heartbeat().init_timer()
         self.dispatcher.emit("init")
 
-        
+
     def insert_page(self, classui, name):
         self.window.insert_page(classui, name, PAGES[name])
         PAGES_LOADED.append(PAGES[name])
@@ -120,3 +120,6 @@ class Main():
 
         logger.info("Changing current working dir (cwd) to {}".format(rectemp))
         os.chdir(rectemp)
+
+    def quit(self):
+        self.window.do_quit()
