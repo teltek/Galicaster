6dba50d6f8095dc96867b9dbccefc58d6e4c4e8a
Alfonso Rodríguez <arodriguez@teltek.es>
2017-01-16 17:31:03 +0100

    Cleaned utils/nautilus.py test from unused imports.

--- tests/utils/nautilus.py
+++ tests/utils/nautilus.py
@@ -6,25 +6,22 @@
 # Copyright (c) 2014, Teltek Video Research <galicaster@teltek.es>
 #
 # This work is licensed under the Creative Commons Attribution-
-# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of 
-# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 171 Second Street, Suite 300, 
+# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
 
 """
 Unit tests for `galicaster.utils.nautilus` module.
 """
-import tempfile
 import subprocess
-from os import path
-from datetime import datetime, timedelta
 
 from unittest import TestCase
-from galicaster.utils import nautilus, systemcalls
+from galicaster.utils import nautilus
 
 class TestFunctions(TestCase):
-    
+
     def setUp(self):
         pass
 
