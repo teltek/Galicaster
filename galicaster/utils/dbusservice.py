c3cd7d99ca4a733b6e7ba3bcb70b48e5c35cf235
Daniel <daniel.peiteado@teltek.es>
2017-03-08 11:33:44 +0100

    Refactor dbusservice to use SystemBus when it's possible

--- galicaster/utils/dbusservice.py
+++ galicaster/utils/dbusservice.py
@@ -6,9 +6,9 @@
 # Copyright (c) 2011, Teltek Video Research <galicaster@teltek.es>
 #
 # This work is licensed under the Creative Commons Attribution-
-# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of 
-# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
-# or send a letter to Creative Commons, 171 Second Street, Suite 300, 
+# NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+# this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+# or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
 """
@@ -20,14 +20,27 @@ import dbus, dbus.glib, dbus.service
 class DBusService(dbus.service.Object):
     def __init__(self, app):
 
-        if dbus.SessionBus().request_name("es.teltek.Galicaster") != dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
-            method = dbus.SessionBus().get_object("es.teltek.Galicaster", "/es/teltek/Galicaster").get_dbus_method("show_window")
-            method()
-            raise SystemError("Galicaster already running")
-        else:
-            self.app = app
-            bus_name = dbus.service.BusName('es.teltek.Galicaster', bus = dbus.SessionBus())
-            dbus.service.Object.__init__(self, bus_name, '/es/teltek/Galicaster')
+        try:
+            if dbus.SystemBus().request_name("es.teltek.Galicaster") != dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
+                method = dbus.SystemBus().get_object("es.teltek.Galicaster", "/es/teltek/Galicaster").get_dbus_method("show_window")
+                method()
+                raise SystemError("Galicaster already running")
+            else:
+                self.app = app
+                bus_name = dbus.service.BusName('es.teltek.Galicaster', bus = dbus.SystemBus())
+                dbus.service.Object.__init__(self, bus_name, '/es/teltek/Galicaster')
+        except Exception as exc:
+            if "Galicaster already running" in exc.args:
+                raise SystemError("Galicaster already running")
+
+            if dbus.SessionBus().request_name("es.teltek.Galicaster") != dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
+                method = dbus.SessionBus().get_object("es.teltek.Galicaster", "/es/teltek/Galicaster").get_dbus_method("show_window")
+                method()
+                raise SystemError("Galicaster already running")
+            else:
+                self.app = app
+                bus_name = dbus.service.BusName('es.teltek.Galicaster', bus = dbus.SessionBus())
+                dbus.service.Object.__init__(self, bus_name, '/es/teltek/Galicaster')
 
     @dbus.service.method(dbus_interface='es.teltek.Galicaster')
     def show_window(self):
