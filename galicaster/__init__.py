573d787a0d9380f6f6cf89bd37a9758b23ead374
Daniel <daniel.peiteado@teltek.es>
2017-07-07 13:16:34 +0200

    Updated version to 2.0.1

--- galicaster/__init__.py
+++ galicaster/__init__.py
@@ -11,7 +11,7 @@
 # or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 import gi
 gi.require_version('Gst', '1.0')
