ac38871d0d4bb24fc886d50f6fb6de276958ac60
Alfonso Rodríguez <arodriguez@teltek.es>
2018-10-17 17:59:32 +0200

    updated version for 2.1.1-RC2

--- galicaster/__init__.py
+++ galicaster/__init__.py
@@ -11,7 +11,7 @@
 # or send a letter to Creative Commons, 171 Second Street, Suite 300,
 # San Francisco, California, 94105, USA.
 
-__version__ = '2.1.1-RC1'
+__version__ = '2.1.1-RC2'
 
 import gi
 gi.require_version('Gst', '1.0')
